e1cc2715aeb9689c0a9b61d2cd6404cf9a0ff62c
Leonid Haidash <Gaydashleo@gmail.com>
2022-12-23 18:14:54 +0200

    paginationAdd

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -2,7 +2,13 @@ const { Contact } = require("../../models");
 
 const getAllContacts = async (req, res) => {
   const { _id } = req.user;
-  const contacts = await Contact.find({ owner: _id });
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find(
+    { owner: _id },
+    "name,email,phone,favorite,owner",
+    { skip, limit: Number(limit) }
+  );
   res.status(200).json({ contacts });
 };
 
