186af7ee905fbde2b30f848bfbcd414a98ca0717
Gaydashleo <Gaydashleo@gmail.com>
2022-12-29 21:00:28 +0200

    resize

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -3,28 +3,43 @@ const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
 
-const atavatarsDir = path.join(__dirname, "../../", "public", "avatars");
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+// const updateAvatar = async (req, res) => {
+//   const { path: tempUpload, originalname } = req.file;
+//   const { _id: id } = req.user;
+//   const imageName = `${id}_${originalname}`;
+//   try {
+//     const resultUpload = path.join(avatarsDir, imageName);
+//     Jimp.read(tempUpload)
+//       .then((avatar) => {
+//         return avatar.resize(250, 250).write(resultUpload);
+//       })
+//       .catch((error) => console.log(error));
+//     await fs.rename(tempUpload, resultUpload);
+//     const avatarURL = path.join("public", "avatars", imageName);
+//     await User.findByIdAndUpdate(req.user._id, { avatarURL });
+//     res.json({ avatarURL });
+//   } catch (error) {
+//     await fs.unlink(tempUpload);
+//     throw error;
+//   }
+// };
 
 const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
-  const { _id: id } = req.user;
-  const imageName = `${id}_${originalname}`;
-  try {
-    const resultUpload = path.join(atavatarsDir, imageName);
-    Jimp.read(tempUpload)
-      .then((avatar) => {
-        return avatar.resize(250, 250).write(resultUpload);
-      })
-      .catch((error) => console.log(error));
-    await fs.rename(tempUpload, resultUpload);
-    // const resizeAvatar = await Jimp.read(resultUpload);
-    // await resizeAvatar.resize(250, 250).write(resultUpload);
-    const avatarURL = path.join("public", "avatars", imageName);
-    await User.findByIdAndUpdate(req.user._id, { avatarURL });
-    res.json({ avatarURL });
-  } catch (error) {
-    await fs.unlink(tempUpload);
-    throw error;
-  }
+
+  const filename = `${_id}.${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  await fs.rename(tempUpload, resultUpload);
+  const resizeAvatar = await Jimp.read(resultUpload);
+  await resizeAvatar.resize(250, 250).write(resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.json({
+    avatarURL,
+  });
 };
 module.exports = updateAvatar;
