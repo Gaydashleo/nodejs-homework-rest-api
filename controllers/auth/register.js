e7f4736772c8b81d50681e8ae73db5e24145acb3
Gaydashleo <Gaydashleo@gmail.com>
2022-12-31 18:05:09 +0200

    end1

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,6 +2,8 @@ const { Conflict } = require("http-errors");
 const gravatar = require("gravatar");
 const { User } = require("../../models/user");
 const bcryptjs = require("bcryptjs");
+const { nanoid } = require("nanoid");
+const { sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { name, email, password, subscription } = req.body;
@@ -11,13 +13,23 @@ const register = async (req, res) => {
   }
   const avatarURL = gravatar.url(email);
   const hashPassword = bcryptjs.hashSync(password, bcryptjs.genSaltSync(10));
+  const verificationToken = nanoid();
   const result = await User.create({
     name,
     email,
     password: hashPassword,
     subscription,
     avatarURL,
+    verificationToken,
   });
+
+  const mail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a href="http://localhost3000/api/users/verify/${verificationToken}">Click verify email</a>`,
+  };
+
+  await sendEmail(mail);
 
   res.status(201).json({
     email: result.email,
